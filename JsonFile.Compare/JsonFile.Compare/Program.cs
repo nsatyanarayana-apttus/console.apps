a7537d23bde082eec2468b719d19d5fec6bea7c4
Nagarajan Satyanarayana <nsatyanarayana@apttus.com>
2020-07-20 12:32:25 +0530

    Added new project

---
+++ JsonFile.Compare/JsonFile.Compare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonFile.Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
