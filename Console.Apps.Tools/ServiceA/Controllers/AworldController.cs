25060c066426b4407c7925d02c91efa393c309a4
Nagarajan Satyanarayana <nsatyanarayana@apttus.com>
2020-08-03 10:08:30 +0530

    Added service API

---
+++ Console.Apps.Tools/ServiceA/Controllers/AworldController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ServiceA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AworldController : ControllerBase
+    {
+        //https://localhost:44340/api/aworld
+        // GET api/aworld
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Hello", "ServiceA - Aworld" };
+        }
+
+        // GET api/aworld/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var key in Request.Headers.Keys) {
+                sb.AppendLine($"{key} : {Request.Headers[key]}");
+            }
+
+            string ss = sb.ToString();
+
+            return "Hello World";
+        }
+
+        private static string[] CallServiceB(int id)
+        {
+            string url = string.Format("{0}{1}", "https://localhost:44310/api/bworld/", id);
+            string[] result = null;
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                string[] b = response.Content.ReadAsAsync<string[]>().Result;
+                //Console.WriteLine(b);
+                result = b ?? null;
+            }
+            else
+            {
+                result = new string[] { response.StatusCode.ToString(), response.ReasonPhrase };
+            }
+            client.Dispose();
+            return result;
+        }
+
+    }
+}
---
+++ Console.Apps.Tools/ServiceA/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System.Net.Http;
+using Jaeger;
+using Jaeger.Samplers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTracing;
+using OpenTracing.Util;
+using OpenTracing.Contrib.NetCore.CoreFx;
+
+
+namespace ServiceA
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddOpenTracing();
+            services.AddTransient<HttpClient>();
+
+            // Adds the Jaeger Tracer.
+            services.AddSingleton<ITracer>(serviceProvider =>
+            {
+                string serviceName = serviceProvider.GetRequiredService<IHostingEnvironment>().ApplicationName;
+
+                // This will log to a default localhost installation of Jaeger.
+                var tracer = new Tracer.Builder(serviceName)
+                    .WithSampler(new ConstSampler(true))
+                    .Build();
+
+                // Allows code that can't use DI to also access the tracer.
+                GlobalTracer.Register(tracer);
+
+                return tracer;
+            });
+
+
+            // Prevent endless loops when OpenTracing is tracking HTTP requests to Jaeger.
+            services.Configure<HttpHandlerDiagnosticOptions>(options =>
+            {
+                options.IgnorePatterns.Add(x => !x.RequestUri.IsLoopback);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
