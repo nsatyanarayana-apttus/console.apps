f6281a3144f44d1f83380ed19be9999678da2bc1
Nagarajan Satyanarayana <nsatyanarayana@apttus.com>
2020-12-21 12:53:53 +0530

    quartz changes

---
+++ QuartzPoc/HelloJob.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzPoc
+{
+    class HelloJob : IJob
+	{
+		public async Task Execute(IJobExecutionContext context)
+		{
+			JobKey key = context.JobDetail.Key;
+            Console.WriteLine(key.Name);
+			await Console.Out.WriteLineAsync("Greetings from HelloJob!");
+		}
+
+    }
+}
--- QuartzPoc/Program.cs
+++ QuartzPoc/Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
 
 namespace QuartzPoc
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program prog = new Program();
+            await prog.WithCronDemo();
+        }
+
+        public async Task WithCronDemo()
+        {
+            Console.WriteLine("Hello Schedular!");
+            // construct a scheduler factory
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler scheduler = await factory.GetScheduler();
+            await scheduler.Start();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<HelloJob>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger3", "group1")
+                    .WithCronSchedule("0/5 * * * * ?")
+                    .ForJob("myJob", "group1")
+                    .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+            Console.ReadLine();
+        }
+
+        public async Task NormalSchedularDemo()
+        {
+            Console.WriteLine("Hello Schedular!");
+            // construct a scheduler factory
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler scheduler = await factory.GetScheduler();
+            await scheduler.Start();
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<HelloJob>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+              .WithIdentity("myTrigger", "group1")
+              .StartNow()
+              .WithSimpleSchedule(x => x
+                  .WithIntervalInSeconds(10)
+                  .WithRepeatCount(3))
+              .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+            Console.ReadLine();
         }
     }
 }
