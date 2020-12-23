d6d7975084c30ab8d60e5be429930687a5ad4e81
Nagarajan Satyanarayana <nsatyanarayana@apttus.com>
2020-12-23 10:58:10 +0530

    poc code

---
+++ SchedularPoc/Jobs/TriggerJob.cs
@@ -0,0 +1,31 @@
+﻿using Quartz;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SchedularPoc.Jobs
+{
+    public class TriggerJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            string url = "https://localhost:44392/api/demo";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Parse the response body.
+                    string b = await response.Content.ReadAsStringAsync();
+                    //Console.WriteLine(b);
+                    Debug.WriteLine(b);
+                }
+                else
+                {
+                    Debug.WriteLine(response.StatusCode.ToString()+"  "+ response.ReasonPhrase);
+                }
+            }
+
+        }
+    }
+}
---
+++ Service/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Message from another service";
+        }
+    }
+}
