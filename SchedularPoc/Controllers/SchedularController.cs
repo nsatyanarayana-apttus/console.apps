e81f947251bfb2533054cf4cf5c415182d1c4736
Nagarajan Satyanarayana <nsatyanarayana@apttus.com>
2020-12-23 09:40:02 +0530

    poc changes

---
+++ SchedularPoc/Controllers/SchedularController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quartz;
+using Quartz.Impl;
+using SchedularPoc.Jobs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SchedularPoc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchedularController : ControllerBase
+    {
+        IScheduler scheduler;
+        public SchedularController()
+        {
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler result = factory.GetScheduler().Result;
+            scheduler = result;
+            scheduler.Start();
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetAsync(string name)
+        {
+            JobKey key = new JobKey(name, "group1");
+            bool isExist = await scheduler.CheckExists(key);
+            if (isExist)
+            {
+                IJobDetail job = await scheduler.GetJobDetail(key);
+                //return Ok(job.Description+job.Key.ToString());
+                return Ok(job.ToString());
+            }
+            else
+            {
+                return NotFound("Job does not exist with given key:" + name);
+            }
+        }
+
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteAsync(string name)
+        {
+            JobKey key = new JobKey(name, "group1");
+            bool isExist = await scheduler.CheckExists(key);
+            if (isExist)
+            {
+                await scheduler.DeleteJob(key);
+                return Ok("Job Deleted");
+            }
+            else
+            {
+                return NotFound("Job does not exist with given key:" + name);
+            }
+        }
+
+        [HttpPost("createjob/{name}")]
+        public async Task<IActionResult> PostAsync(string name, Dictionary<string, string> dataMap)
+        {
+            JobKey key = new JobKey(name, "group1");
+            bool isExist = await scheduler.CheckExists(key);
+            if (!isExist)
+            {
+                // define the job and tie it to our HelloJob class
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity(name, "group1")
+                    .Build();
+
+                // Trigger the job to run now, and then every 40 seconds
+                ITrigger trigger = TriggerBuilder.Create()
+                  .WithIdentity("myTrigger", "group1")
+                  .StartNow()
+                  .WithSimpleSchedule(x => x
+                      .WithIntervalInSeconds(5)
+                      .WithRepeatCount(100))
+                  .Build();
+
+                await scheduler.ScheduleJob(job, trigger);
+                return Ok("Job Created");
+            }
+            else
+            {
+                return Ok("Job already exist with given key:" + name);
+            }
+
+
+        }
+
+    }
+}
---
+++ SchedularPoc/Jobs/HelloJob.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SchedularPoc.Jobs
+{
+    class HelloJob : IJob
+	{
+		public async Task Execute(IJobExecutionContext context)
+		{
+			JobKey key = context.JobDetail.Key;
+            Console.WriteLine(key.Name);
+			Debug.WriteLine("Greetings from HelloJob!");
+			await Console.Out.WriteLineAsync("Ok");
+		}
+
+    }
+}
