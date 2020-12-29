306b03a2dacecb2ef24e5fad5e6c4bad10c83498
Nagarajan Satyanarayana <nsatyanarayana@apttus.com>
2020-12-29 11:51:10 +0530

    changes

--- MongoDb/Program.cs
+++ MongoDb/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 
 namespace MongoDb
 {
@@ -7,6 +8,20 @@ namespace MongoDb
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string cs = "mongodb://root:Apttu#cms#engg#123@ussc-cms-eng-mdb-dev-01.apttuscloud.io:27017,ussc-cms-eng-mdb-dev-03.apttuscloud.io:27017,ussc-cms-eng-mdb-dev-02.apttuscloud.io:27017/?authSource=admin&replicaSet=replicaset&readPreference=primary&appname=MongoDB Compass&ssl=false";
+            //MongoClient dbClient = new MongoClient("mongodb://root:Apttu%23cms%23engg%23123@ussc-cms-eng-mdb-dev-01.apttuscloud.io:27017,ussc-cms-eng-mdb-dev-03.apttuscloud.io:27017,ussc-cms-eng-mdb-dev-02.apttuscloud.io:27017/?authSource=admin&replicaSet=replicaset&readPreference=primary&appname=MongoDB%20Compass&ssl=false");
+            MongoClient dbClient = new MongoClient(cs);
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+
+            Console.ReadLine();
         }
     }
 }
